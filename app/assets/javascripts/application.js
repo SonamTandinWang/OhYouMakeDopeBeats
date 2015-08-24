ca882232e8cf683c63079e9871658d34856c928d
Sonam Tandin Wong <sonamtandinwong@gmail.com>
2015-08-24 12:11:56 -0400

    Backbone directory added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/oh_you_make_dope_beats
 //= require_tree .
